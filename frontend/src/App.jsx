914b7eb2819a7735bee812ae3058e3d91c7f289f
sergioesenebe <sergio.nunez@esenebe.com>
2025-06-23 17:42:47 +0200

    More Comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
+//Import React Library
 import React from 'react';
-
+//App function
 function App() {
+  //Return html
   return (
     <div>
       <h1>MyTrip Frontend</h1>
     </div>
   );
 }
-
+//Export App
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
+//Import React
 import React from 'react';
+//Import ReactDOM to render react components in the DOM
 import ReactDOM from 'react-dom/client';
+//Import main App component
 import App from './App';
 
+//Render in root the App component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
