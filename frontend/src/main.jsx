4912fa8d4b9ce5b6c77033b651c2430150a203b6
sergioesenebe <sergio.nunez@esenebe.com>
2025-08-19 17:39:53 +0200

    healmet

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,18 @@ import ReactDOM from 'react-dom/client';
 //Import main App component
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+//Import helmet
+import { HelmetProvider } from "react-helmet-async";
 //Import Tailwind library
 import './styles/index.css'
 
 //Render in root the App component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App /> 
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
