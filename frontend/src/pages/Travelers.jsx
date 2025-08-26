33e42b58f9d1407cd03733d3ac09340abdd9eaa9
sergioesenebe <sergio.nunez@esenebe.com>
2025-08-26 17:10:03 +0200

    mobile fit

--- frontend/src/pages/Travelers.jsx
+++ frontend/src/pages/Travelers.jsx
@@ -300,7 +300,7 @@ function travelers() {
     return (
         <>
             <Helmet>
-                <meta charSet="utf-8" />
+                <meta charSet="utf-8" name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>myTrip - Travelers</title>
                 <link rel="icon" href={logo} />
             </Helmet>
