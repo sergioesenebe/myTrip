4480aba48f012dd9a107ccfc0d42e65c1f6ddbb0
sergioesenebe <sergio.nunez@esenebe.com>
2026-06-02 17:00:18 +0200

    Add 'place-image' class to trip avatar image

--- frontend/src/pages/Trips.jsx
+++ frontend/src/pages/Trips.jsx
@@ -602,7 +602,7 @@ function trips() {
                                             <p>{trip.description}</p>
                                             <p>{trip.country}, {trip.city}</p>
                                             <div className='flex flex-row gap-[10px] items-center'>
-                                                <img src={trip.avatar} className='w-[35px] h-[35px] border border-white rounded-full aspect-square' />
+                                                <img src={trip.avatar} className='w-[35px] h-[35px] border border-white rounded-full aspect-square place-image' />
                                                 <p>{trip.username}</p>
                                             </div>
                                         </div>
